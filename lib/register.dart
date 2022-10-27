4ce5bd4c1e80183271d8b521f2c43eaa3dc8621d
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2022-10-27 09:10:55 +0200

    .

--- lib/register.dart
+++ lib/register.dart
@@ -27,6 +27,86 @@ class _RegisterPageState extends State<RegisterPage> {
   TextEditingController passwordController = TextEditingController();
 
   // -- Methods --
+  /// It creates an account
+  ///
+  /// Returns:
+  ///   A Future<void>
+  createAAccount() async {
+    {
+      // Create loading dialog
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      );
+
+      // Check if the username and email is already taken
+      var username = usernameController.text;
+      var email = emailController.text;
+      var password = passwordController.text;
+
+      // Check if all fields are not empty
+      if (username.isEmpty || email.isEmpty || password.isEmpty) {
+        // Close loading dialog
+        Navigator.of(context).pop();
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Veuillez remplir tous les champs')));
+        return;
+      }
+
+      // Check the email with regex
+      if (!RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email)) {
+        // Close the loading dialog
+        Navigator.pop(context);
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('L\'adresse email n\'est pas valide')));
+        return;
+      }
+
+      var usernameQuery = await widget.db
+          .collection('user')
+          .find(MongoDatabase.searchWhere('username', username))
+          .toList();
+      var emailQuery = await widget.db
+          .collection('user')
+          .find(MongoDatabase.searchWhere('email', email))
+          .toList();
+
+      if (usernameQuery.length > 0) {
+        // Username already taken
+        Navigator.pop(context);
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Ce nom d\'utilisateur est déjà pris')));
+        return;
+      }
+
+      if (emailQuery.length > 0) {
+        // Email already taken
+        Navigator.pop(context);
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Cette adresse email est déjà prise')));
+        return;
+      }
+
+      // Add the user to the database
+      await widget.db.collection('user').insertOne(<String, dynamic>{
+        'accountCreatedAt': DateTime.now(),
+        'username': username,
+        'email': email,
+        'password': password,
+      });
+
+      // Close the loading dialog
+      Navigator.of(context).pop();
+
+      // Go to the login page
+      Navigator.pop(context);
+    }
+  }
 
   // -- Widgets --
   @override
